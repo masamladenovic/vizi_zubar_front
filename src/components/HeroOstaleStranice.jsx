1f52f3fd66e04045bbaa8239455bcf0673265ca3
masha <masamladenovic06@gmail.com>
2026-08-28 11:22:28 +0200

    Dodat Breadcrumbs

--- src/components/HeroOstaleStranice.jsx
+++ src/components/HeroOstaleStranice.jsx
@@ -1,10 +1,12 @@
 import "./CSS/HeroOstaleStranice.css";
+import {Breadcrumbs} from "./navigacija/Breadcrumbs"
 
 const HeroOstaleStranice = ({ naslov }) => {
   return (
     <section>
       <div className="hero-ostale-stranice">
         <h1 className="title">{naslov}</h1>
+        <Breadcrumbs/>
       </div>
     </section>
   );
---
+++ src/components/navigacija/Breadcrumbs.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import useBreadcrumbs from "use-react-router-breadcrumbs";
+const routes = [
+  { path: "/", breadcrumb: "Home" },
+  { path: "/about", breadcrumb: "About Us" },
+  { path: "/dentists", breadcrumb: "Dentists" },
+  { path: "/services", breadcrumb: "Services" },
+];
+
+export const Breadcrumbs = () => {
+  const breadcrumbs = useBreadcrumbs(routes);
+
+  return (
+    <div className="breadcrumbs">
+      {breadcrumbs.map(({ match, breadcrumb }, index) => (
+        <span key={match.pathname}>
+          <Link to={match.pathname}>{breadcrumb}</Link>
+          {index < breadcrumbs.length - 1 && " / "}
+        </span>
+      ))}
+    </div>
+  );
+};
